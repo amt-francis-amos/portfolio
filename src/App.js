f6bca35b8dbea6923950ec210df7a8cff4731103
francis.amos123 <fmamos@st.nsbt.edu.gh>
2023-10-15 13:37:04 +0000

    commit to main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Intro from "./components/Intro/Intro";
 
 
 function App() {
   return (
     <div className="App">
         <Navbar />
+        <Intro />
     </div>
   );
 }
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './intro.css';
+import bg from  '../../assets/image.png';
+import btnImg from  '../../assets/hireme.png';
+import {Link} from 'react-scroll';
 
 const Intro = () => {
   return (
-    <div>
-      
-    </div>
+     <section id="intro">
+        <div className="intro-content">
+            <span className="hello">Hello,</span>
+            <span className="introText">I'am <span className="introName">Amos</span> <br />Website Designer</span>
+        <p className="introInfo">I am a skilled web designer with experience in creating <br />visually appealing and user-friendly websites.</p>
+        <Link><button className='btn'><img src={btnImg} alt="/" className='btnImg' />Hire Me</button></Link>
+        </div>
+        <img src={bg} alt="Profile" className="bg" />
+     </section> 
   )
 }
 
